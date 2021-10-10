d0e3029ab312eb342626af2bbdff474ebb00b86e
wbedu <23764568+wbedu@users.noreply.github.com>
2021-10-09 20:20:51 -0500

    user let and const rather than var

--- test/sqlite/connect.test.js
+++ test/sqlite/connect.test.js
@@ -10,52 +10,54 @@ try {
   // do nothing if it does not exists
 }
 
-test('db connection resolves without error', async () => {
-  let error = false
-  try {
+describe('sqlite', function () {
+  test('db connection resolves without error', async () => {
+    let error = false
+    try {
+      const db = await connect()
+      db.close()
+    } catch (e) {
+      error = true
+    }
+    expect(error).toBeFalsy()
+  })
+
+  test('db allows table creation', async () => {
     const db = await connect()
-    db.close()
-  } catch (e) {
-    error = true
-  }
-  expect(error).toBeFalsy()
-})
+    let error = false
+    try {
+      await db.exec('CREATE TABLE tbl (col TEXT)')
+      db.close()
+    } catch (e) {
+      error = true
+    }
+    expect(error).toBeFalsy()
+  })
 
-test('db allows table creation', async () => {
-  const db = await connect()
-  let error = false
-  try {
-    await db.exec('CREATE TABLE tbl (col TEXT)')
-    db.close()
-  } catch (e) {
-    error = true
-  }
-  expect(error).toBeFalsy()
-})
+  test('db allows table writes', async () => {
+    const db = await connect()
+    let error = false
+    try {
+      await db.exec('INSERT INTO tbl VALUES ("test")')
+      db.close()
+    } catch (e) {
+      error = true
+    }
+    expect(error).toBeFalsy()
+  })
 
-test('db allows table writes', async () => {
-  const db = await connect()
-  let error = false
-  try {
-    await db.exec('INSERT INTO tbl VALUES ("test")')
-    db.close()
-  } catch (e) {
-    error = true
-  }
-  expect(error).toBeFalsy()
-})
-
-test('db allows table reads', async () => {
-  const db = await connect()
-  let error = false
-  let result = null
-  try {
-    result = await db.all('SELECT * FROM tbl')
-    db.close()
-  } catch (e) {
-    error = true
-  }
-  expect(error).toBeFalsy()
-  expect(result.length).toBe(1)
-  expect(result[0].col).toBe('test')
+  test('db allows table reads', async () => {
+    const db = await connect()
+    let error = false
+    let result = null
+    try {
+      result = await db.all('SELECT * FROM tbl')
+      db.close()
+    } catch (e) {
+      error = true
+    }
+    expect(error).toBeFalsy()
+    expect(result.length).toBe(1)
+    expect(result[0].col).toBe('test')
+  })
 })
