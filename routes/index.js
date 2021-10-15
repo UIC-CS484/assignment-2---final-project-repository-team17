6d4ad808ab921db71af7d426f566d697f9f03812
MOnishaSuthapalli <MOnishaSuthapalli688@gmail.com>
2021-10-14 23:12:00 -0500

    test case root

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,14 @@ router.get('/', function (req, res, next) {
   res.render('root')
 })
 
-router.post('/login', function (req, res, next) {
+router.get('/login', function (req, res, next) {
   res.render('login')
 })
+
+router.post('/login', function (req, res, next) {
+  res.render('login', { loginError: 'Invalid Credentials.' })
+})
+
 
 router.get('/settings', function (req, res, next) {
   res.render('settings')
@@ -17,5 +22,15 @@ router.get('/settings', function (req, res, next) {
 router.get('/registration', function (req, res, next) {
   res.render('signup')
 })
+
+router.post('/registration', function (req, res, next) {
+  res.render('signup', {
+    unameError: 'Invalid Username',
+    passwordError:'Invalid Password',
+    emailError:'Invalid Email'
+  })
+})
+
+
 
 module.exports = router
