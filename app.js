ab723d61daa2d252b5e2dfbd17720f379fa7330c
MOnishaSuthapalli <MOnishaSuthapalli688@gmail.com>
2021-10-14 20:50:05 -0500

    handlebars ui

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
+// const handlebars = require('express-handlebars');
 
 const indexRouter = require('./routes/index')
 const usersRouter = require('./routes/users')
@@ -10,8 +11,9 @@ const usersRouter = require('./routes/users')
 const app = express()
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'hbs');
+
 
 app.use(logger('dev'))
 app.use(express.json())
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,19 @@ const router = express.Router()
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'TrackItUp' })
+  res.render('root')
+})
+
+router.post('/login', function (req, res, next) {
+  res.render('login')
+})
+
+router.get('/settings', function (req, res, next) {
+  res.render('settings')
+})
+
+router.get('/registration', function (req, res, next) {
+  res.render('signup')
 })
 
 module.exports = router
