15b233738cb9c2b209d23c80a7964363ae1ac5d1
null <null>
2021-10-20 23:17:03 -0500

    authenticates user after creation

--- routes/index/registration.js
+++ routes/index/registration.js
@@ -4,22 +4,41 @@ const router = express.Router()
 const { createUser } = require('../../dao/users')
 
 router.get('/', function (req, res, next) {
-  res.render('signup')
+  if (req.isUnauthenticated()) {
+    res.render('signup')
+  } else {
+    return res.redirect('settings')
+  }
+  return next()
 })
 
 router.post('/', async (req, res, next) => {
   await createUser(req.body.email, req.body.password, req.body.username, (error) => {
     if (error === undefined) {
-      res.status(201).render('settings')
+      const user = {
+        email: req.body.email,
+        username: req.body.username
+      }
+      req.login(user, function (err) {
+        if (err) { return next(err) }
+        res.status(201).redirect('/settings')
+      })
     } else if (error.error && error.error === 'User Error') {
-      console.error('user error', error)
       res.status(406).render('signup', {
         body: JSON.stringify(req.body),
         ...error
       })
     } else {
-      console.error('sql error', error)
-      const displayErrors = parseSQlErrors(error)
+      const displayErrors = {}
+
+      if (error.message === 'SQLITE_CONSTRAINT: UNIQUE constraint failed: users.email') {
+        displayErrors.emailError = 'This username is not available for use'
+      } else if (error.message === 'SQLITE_CONSTRAINT: UNIQUE constraint failed: users.username') {
+        displayErrors.emailError = 'This email is not available for use'
+      } else {
+        displayErrors.generalError = 'Something went wrong. Please refresh and try again.'
+      }
+
       res.status(406).render('signup', {
         ...displayErrors
       })
@@ -27,17 +46,4 @@ router.post('/', async (req, res, next) => {
   })
 })
 
-function parseSQlErrors (error) {
-  const display = {}
-
-  if (error.message === 'SQLITE_CONSTRAINT: UNIQUE constraint failed: users.email') {
-    display.emailError = 'This username is not available for use'
-  } else if (error.message === 'SQLITE_CONSTRAINT: UNIQUE constraint failed: users.username') {
-    display.emailError = 'This email is not available for use'
-  } else {
-    display.generalError = 'Something went wrong. Please refresh and try again.'
-  }
-
-  return display
-}
 module.exports = router
