3dbf24fd8c7644d7bf3d51dcd09f417a2461631d
MOnishaSuthapalli <MOnishaSuthapalli688@gmail.com>
2021-10-21 01:42:41 -0500

    added logger

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
 const createError = require('http-errors')
+const fs=require('fs')
 const express = require('express')
 const path = require('path')
 const cookieParser = require('cookie-parser')
-const logger = require('morgan')
+const morgan = require('morgan')
+// const logger=require('./logger')
 // const handlebars = require('express-handlebars');
 
 const indexRouter = require('./routes/index')
 const usersRouter = require('./routes/users')
+const accessLogStream = fs.createWriteStream(path.join(__dirname, './logger/error.log'), { flags: 'a' })
+
 
 const app = express()
+
 
 // view engine setup
 app.set('views', path.join(__dirname, '/views'))
 app.set('view engine', 'hbs')
 
-app.use(logger('dev'))
+
+// app.use(logger('dev'))
+app.use(morgan('combined', { stream: accessLogStream }))
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
@@ -38,5 +46,7 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500)
   res.render('error')
 })
+
+
 
 module.exports = app
