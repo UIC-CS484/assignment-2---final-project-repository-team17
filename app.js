e28463f335399e2fc0a41395529dd04fd2b999c6
null <null>
2021-10-20 23:22:16 -0500

    authenticates user after creation

--- app.js
+++ app.js
@@ -3,13 +3,28 @@ const express = require('express')
 const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
-// const handlebars = require('express-handlebars');
+const passport = require('passport')
+const session = require('express-session')
 
 const indexRouter = require('./routes/index')
 const usersRouter = require('./routes/users')
 
 const app = express()
 
+const sess = {
+  secret: '97p]_>y~#G#[dCS/',
+  cookie: {}
+}
+
+app.use(session(sess))
+// setup passport
+try {
+  app.use(passport.initialize())
+  app.use(passport.session())
+  require('./passport')()
+} catch (error) {
+  console.error(error)
+}
 // view engine setup
 app.set('views', path.join(__dirname, '/views'))
 app.set('view engine', 'hbs')
