8c8065fc6a8d66230956b25443503e289c6c5325
null <null>
2021-10-22 21:42:41 -0500

    redirect if not logged in

--- routes/index/settings.js
+++ routes/index/settings.js
@@ -2,7 +2,11 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', function (req, res, next) {
-  res.render('settings')
+  if (req.isNotAuthenticated()) {
+    return res.render('signup')
+  } else {
+    res.render('settings')
+  }
 })
 
 module.exports = router
