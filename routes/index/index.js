1b541c25d8189d42ddee8cdba230a658c007fbe5
Will Bedu <23764568+wbedu@users.noreply.github.com>
2021-11-30 21:13:28 -0600

    cors update

--- routes/index/index.js
+++ routes/index/index.js
@@ -3,7 +3,7 @@ const registrationRouter = require('./registration')
 const settingsRouter = require('./settings')
 const loginRouter = require('./login')
 const logoutRouter = require('./logout')
-const cors=require('cors')
+const cors = require('cors')
 const router = express.Router()
 
 /* GET home page. */
@@ -11,31 +11,20 @@ router.get('/', function (req, res, next) {
   res.redirect('/login')
 })
 
-
-router.get('/chart', cors(),function (req, res, next) {
+router.get('/chart', cors(), function (req, res, next) {
   res.render('chart')
 })
 
-
-const whitelist = ['http://localhost:3000/*', 'https://trackitup.net/*']
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error())
-    }
-  }
+const corsConfig = {
+  origin: process.env.NODE_ENV === 'production'
+    ? 'https://trackitup.net/*'
+    : 'http://localhost:3000/*',
+  credentials: true,
+  methods: ['GET', 'HEAD', 'OPTIONS', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  allowedHeaders: ['Content-Type']
 }
 
-const corsConfig = {
-  origin: ["https://trackitup.net/*", "http://localhost:3000/*"],
-  credentials: true,
-  methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
-  allowedHeaders: ['Content-Type']
-};
-
-router.use(cors(corsConfig));
+router.use(cors(corsConfig))
 
 router.use('/login', loginRouter)
 
