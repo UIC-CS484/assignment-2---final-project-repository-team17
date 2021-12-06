521c3c8609faf0f5d82ce7e313bd842521cfd5bb
Will Bedu <23764568+wbedu@users.noreply.github.com>
2021-12-05 22:16:35 -0600

    movies home refactor

--- routes/index/home.js
+++ routes/index/home.js
@@ -1,14 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const axios = require('axios')
-
+const MostPopularMovies = require('../../dao/movies/mostPopularMovies.json')
 router.get('/', async function (req, res) {
   if (req.isAuthenticated()) {
     try {
-      const apiurl = 'https://imdb-api.com/en/API/MostPopularMovies/k_lt7pi174'
-      const response = await axios.get(apiurl)
-
-      res.render('home', { response: response.data })
+      res.render('home', { response: MostPopularMovies })
     } catch (error) {
       return console.log(error)
     }
